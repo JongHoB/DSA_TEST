f990404fe21a936b51bdd24a68d3b870e2da21ea
JongHoB <qorwhdgh@gmail.com>
2024-02-07 11:14:18 +0000

    sva enqcmds shared

--- kernel_test/kernel_test.h
+++ kernel_test/kernel_test.h
@@ -1,5 +1,5 @@
-
-#define NPAGES 1024
+#include "/usr/src/linux-6.8-rc2/drivers/dma/idxd/idxd.h"
+#define NPAGES 1
 
 #ifndef __SO2MMAP_H__
 #define __SO2MMAP_H__ 1
@@ -32,40 +32,176 @@
 
 struct dma_chan *dma_get_slave_channel(struct dma_chan *chan);
 
-// static inline struct idxd_device_driver *wq_to_idxd_drv(struct idxd_wq *wq)
-// {
-// 	struct device *dev = wq_confdev(wq);
-// 	struct idxd_device_driver *idxd_drv =
-// 		container_of(dev->driver, struct idxd_device_driver, drv);
+static inline bool idxd_device_is_halted(struct idxd_device *idxd)
+{
+	union gensts_reg gensts;
 
-// 	return idxd_drv;
-// }
+	gensts.bits = ioread32(idxd->reg_base + IDXD_GENSTATS_OFFSET);
 
-// static inline unsigned char enqcmd(void *dst, const void *src)
-// {
-// 	unsigned char retry;
+	return (gensts.state == IDXD_DEVICE_STATE_HALT);
+}
 
-// 	asm volatile(".byte 0xf2, 0x0f, 0x38, 0xf8, 0x02\t\n"
-// 			"setz %0\t\n"
-// 			: "=r"(retry) : "a" (dst), "d" (src));
-// 	return retry;
-// }
+static void idxd_cmd_exec(struct idxd_device *idxd, int cmd_code, u32 operand,
+						  u32 *status)
+{
+	union idxd_command_reg cmd;
+	DECLARE_COMPLETION_ONSTACK(done);
+	u32 stat;
+	unsigned long flags;
 
-// static inline void movdir64b(void *dst, const void *src)
-// {
-// 	asm volatile(".byte 0x66, 0x0f, 0x38, 0xf8, 0x02\t\n"
-// 		: : "a" (dst), "d" (src));
-// }
+	if (idxd_device_is_halted(idxd))
+	{
+		dev_warn(&idxd->pdev->dev, "Device is HALTED!\n");
+		if (status)
+			*status = IDXD_CMDSTS_HW_ERR;
+		return;
+	}
 
-// static __always_inline int umwait(unsigned long timeout, unsigned int state)
-// {
-// 	uint8_t r;
-// 	uint32_t timeout_low = (uint32_t)timeout;
-// 	uint32_t timeout_high = (uint32_t)(timeout >> 32);
+	memset(&cmd, 0, sizeof(cmd));
+	cmd.cmd = cmd_code;
+	cmd.operand = operand;
+	cmd.int_req = 1;
 
-// 	asm volatile(".byte 0xf2, 0x48, 0x0f, 0xae, 0xf1\t\n"
-// 				 "setc %0\t\n"
-// 				 : "=r"(r)
-// 				 : "c"(state), "a"(timeout_low), "d"(timeout_high));
-// 	return r;
-// }
+	spin_lock_irqsave(&idxd->cmd_lock, flags);
+	wait_event_lock_irq(idxd->cmd_waitq,
+						!test_bit(IDXD_FLAG_CMD_RUNNING, &idxd->flags),
+						idxd->cmd_lock);
+
+	dev_dbg(&idxd->pdev->dev, "%s: sending cmd: %#x op: %#x\n",
+			__func__, cmd_code, operand);
+
+	idxd->cmd_status = 0;
+	__set_bit(IDXD_FLAG_CMD_RUNNING, &idxd->flags);
+	idxd->cmd_done = &done;
+	iowrite32(cmd.bits, idxd->reg_base + IDXD_CMD_OFFSET);
+
+	/*
+	 * After command submitted, release lock and go to sleep until
+	 * the command completes via interrupt.
+	 */
+	spin_unlock_irqrestore(&idxd->cmd_lock, flags);
+	wait_for_completion(&done);
+	stat = ioread32(idxd->reg_base + IDXD_CMDSTS_OFFSET);
+	spin_lock(&idxd->cmd_lock);
+	if (status)
+		*status = stat;
+	idxd->cmd_status = stat & GENMASK(7, 0);
+
+	__clear_bit(IDXD_FLAG_CMD_RUNNING, &idxd->flags);
+	/* Wake up other pending commands */
+	wake_up(&idxd->cmd_waitq);
+	spin_unlock(&idxd->cmd_lock);
+}
+
+static void idxd_wq_disable_cleanup(struct idxd_wq *wq)
+{
+	struct idxd_device *idxd = wq->idxd;
+
+	lockdep_assert_held(&wq->wq_lock);
+	wq->state = IDXD_WQ_DISABLED;
+	memset(wq->wqcfg, 0, idxd->wqcfg_size);
+	wq->type = IDXD_WQT_NONE;
+	wq->threshold = 0;
+	wq->priority = 0;
+	wq->enqcmds_retries = IDXD_ENQCMDS_RETRIES;
+	wq->flags = 0;
+	memset(wq->name, 0, WQ_NAME_SIZE);
+	wq->max_xfer_bytes = WQ_DEFAULT_MAX_XFER;
+	idxd_wq_set_max_batch_size(idxd->data->type, wq, WQ_DEFAULT_MAX_BATCH);
+	if (wq->opcap_bmap)
+		bitmap_copy(wq->opcap_bmap, idxd->opcap_bmap, IDXD_MAX_OPCAP_BITS);
+}
+
+int idxd_wq_enable(struct idxd_wq *wq)
+{
+	struct idxd_device *idxd = wq->idxd;
+	struct device *dev = &idxd->pdev->dev;
+	u32 status;
+
+	if (wq->state == IDXD_WQ_ENABLED)
+	{
+		dev_dbg(dev, "WQ %d already enabled\n", wq->id);
+		return 0;
+	}
+
+	idxd_cmd_exec(idxd, IDXD_CMD_ENABLE_WQ, wq->id, &status);
+
+	if (status != IDXD_CMDSTS_SUCCESS &&
+		status != IDXD_CMDSTS_ERR_WQ_ENABLED)
+	{
+		dev_dbg(dev, "WQ enable failed: %#x\n", status);
+		return -ENXIO;
+	}
+
+	wq->state = IDXD_WQ_ENABLED;
+	set_bit(wq->id, idxd->wq_enable_map);
+	dev_dbg(dev, "WQ %d enabled\n", wq->id);
+	return 0;
+}
+
+int idxd_wq_disable(struct idxd_wq *wq, bool reset_config)
+{
+	struct idxd_device *idxd = wq->idxd;
+	struct device *dev = &idxd->pdev->dev;
+	u32 status, operand;
+
+	dev_dbg(dev, "Disabling WQ %d\n", wq->id);
+
+	if (wq->state != IDXD_WQ_ENABLED)
+	{
+		dev_dbg(dev, "WQ %d in wrong state: %d\n", wq->id, wq->state);
+		return 0;
+	}
+
+	operand = BIT(wq->id % 16) | ((wq->id / 16) << 16);
+	idxd_cmd_exec(idxd, IDXD_CMD_DISABLE_WQ, operand, &status);
+
+	if (status != IDXD_CMDSTS_SUCCESS)
+	{
+		dev_dbg(dev, "WQ disable failed: %#x\n", status);
+		return -ENXIO;
+	}
+
+	if (reset_config)
+		idxd_wq_disable_cleanup(wq);
+	clear_bit(wq->id, idxd->wq_enable_map);
+	wq->state = IDXD_WQ_DISABLED;
+	dev_dbg(dev, "WQ %d disabled\n", wq->id);
+	return 0;
+}
+
+static void __idxd_wq_set_pasid_locked(struct idxd_wq *wq, int pasid)
+{
+	struct idxd_device *idxd = wq->idxd;
+	union wqcfg wqcfg;
+	unsigned int offset;
+
+	offset = WQCFG_OFFSET(idxd, wq->id, WQCFG_PASID_IDX);
+	spin_lock(&idxd->dev_lock);
+	wqcfg.bits[WQCFG_PASID_IDX] = ioread32(idxd->reg_base + offset);
+	wqcfg.pasid_en = 1;
+	wqcfg.pasid = pasid;
+	wq->wqcfg->bits[WQCFG_PASID_IDX] = wqcfg.bits[WQCFG_PASID_IDX];
+	iowrite32(wqcfg.bits[WQCFG_PASID_IDX], idxd->reg_base + offset);
+	spin_unlock(&idxd->dev_lock);
+}
+
+int idxd_wq_set_pasid(struct idxd_wq *wq, int pasid)
+{
+	int rc;
+
+	rc = idxd_wq_disable(wq, false);
+	pr_info("idxd_wq_disable: %d\n", rc);
+	if (rc < 0)
+		return rc;
+
+	__idxd_wq_set_pasid_locked(wq, pasid);
+
+	pr_info("idxd_wq_set\n");
+
+	rc = idxd_wq_enable(wq);
+	if (rc < 0)
+		return rc;
+
+	return 0;
+}
--- kernel_test/kernel_test_dmaengine.c
+++ kernel_test/kernel_test_dmaengine.c
@@ -35,198 +35,12 @@ MODULE_IMPORT_NS(IDXD);
 static struct dma_chan *chan = NULL;
 static struct idxd_wq *wq;
 struct idxd_device *idxd_device;
-// static struct idxd_device *idxd_device;
-// static struct idxd_dma_chan *idxd_chan;
-
-#define MY_MAJOR 42
-
-/* character device basic structure */
-// static struct cdev mmap_cdev;
 
 /* pointer to the vmalloc'd area, rounded up to a page boundary */
 static char *kmalloc_area;
 static char *kmalloc_area2;
-
-static int my_open(struct inode *inode, struct file *filp)
-{
-    return 0;
-}
-
-static int my_release(struct inode *inode, struct file *filp)
-{
-    return 0;
-}
-
-// Read from vmalloc_area to user_buffer
-// This function is called when read() is called in user space
-static ssize_t my_read(struct file *file, char __user *user_buffer,
-                       size_t size, loff_t *offset)
-{
-    /* TODO 2/2: check size doesn't exceed our mapped area size */
-    if (size > NPAGES * PAGE_SIZE)
-        size = NPAGES * PAGE_SIZE;
-
-    /* TODO 2/2: copy from mapped area to user buffer */
-    if (copy_to_user(user_buffer, kmalloc_area, size))
-        return -EFAULT;
-
-    return size;
-}
-
-// Write from user_buffer to vmalloc_area
-// This function is called when write() is called in user space
-static ssize_t my_write(struct file *file, const char __user *user_buffer,
-                        size_t size, loff_t *offset)
-{
-    /* TODO 2/2: check size doesn't exceed our mapped area size */
-    if (size > NPAGES * PAGE_SIZE)
-        size = NPAGES * PAGE_SIZE;
-
-    /* TODO 2/3: copy from user buffer to mapped area */
-    memset(kmalloc_area, 0, NPAGES * PAGE_SIZE);
-    if (copy_from_user(kmalloc_area, user_buffer, size))
-        return -EFAULT;
-
-    return size;
-}
-
-// Map vmalloc_area to user space
-// This function is called when mmap() is called in user space
-// vm_area_struct is a structure that represents a memory mapping
-// vmalloc_to_pfn() returns the page frame number of a vmalloc'd area
-// remap_pfn_range() maps a page frame number to a user space address
-// Then when the user access the user space virtual address through mmap(),
-// the kernel will translate the virtual address to the physical address
-static int my_mmap(struct file *filp, struct vm_area_struct *vma)
-{
-    int ret;
-    long length = vma->vm_end - vma->vm_start; // length of mapping
-    unsigned long start = vma->vm_start;       // start address of mapping
-    char *kmalloc_area_ptr = kmalloc_area;     // pointer to kmalloc_area
-    unsigned long pfn;
-
-    if (length > NPAGES * PAGE_SIZE) // check length
-        return -EIO;
-
-    pfn = page_to_pfn(virt_to_page(kmalloc_area_ptr));                 // get page frame number
-    ret = remap_pfn_range(vma, start, pfn, length, vma->vm_page_prot); // map page frame number to user space
-    if (ret < 0)
-        return ret;
-
-    return 0;
-}
-
-static const struct file_operations mmap_fops = {
-    .owner = THIS_MODULE,
-    .open = my_open,
-    .release = my_release,
-    .mmap = my_mmap,
-    .read = my_read,
-    .write = my_write};
-
-// This function is called when cat /proc/mymap is called in user space
-// seq_file is a structure that represents a file in /proc
-static int my_seq_show(struct seq_file *seq, void *v)
-{
-    struct mm_struct *mm;
-    struct vm_area_struct *vma_iterator;
-    unsigned long total = 0;
-
-// if linux version is less than 6.1, use mm->mmap
-#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 1, 0)
-
-    /* TODO 3: Get current process' mm_struct */
-    mm = get_task_mm(current);
-
-    /* TODO 3/6: Iterate through all memory mappings and print ranges */
-    vma_iterator = mm->mmap;
-    while (vma_iterator != NULL)
-    {
-        pr_info("%lx %lx\n", vma_iterator->vm_start, vma_iterator->vm_end);
-        total += vma_iterator->vm_end - vma_iterator->vm_start;
-        vma_iterator = vma_iterator->vm_next;
-    }
-#else
-    mm = get_task_mm(current);
-
-    VMA_ITERATOR(vmi, mm, 0);
-
-    for_each_vma(vmi, vma_iterator)
-    {
-        pr_info("%lx %lx\n", vma_iterator->vm_start, vma_iterator->vm_end);
-        total += vma_iterator->vm_end - vma_iterator->vm_start;
-    }
-#endif
-
-    /* TODO 3: Release mm_struct */
-    mmput(mm);
-
-    /* TODO 3: write the total count to file  */
-    seq_printf(seq, "%lu %s\n", total, current->comm);
-    return 0;
-}
-
-// This function is called when cat /proc/mymap is called in user space
-static int my_seq_open(struct inode *inode, struct file *file)
-{
-    /* TODO 3: Register the display function */
-    return single_open(file, my_seq_show, NULL);
-}
-
-static const struct proc_ops my_proc_ops = {
-    .proc_open = my_seq_open,
-    .proc_read = seq_read,
-    .proc_lseek = seq_lseek,
-    .proc_release = single_release,
-};
-
-// static int load_dsa_device2(struct idxd_device *idxd)
-// {
-//     struct idxd_engine *engine;
-//     struct idxd_group *group;
-
-//     if (!test_bit(IDXD_FLAG_CONFIGURABLE, &idxd->flags))
-//         return 0;
-
-//     wq = idxd->wqs[1];
-
-//     if (wq->state != IDXD_WQ_DISABLED)
-//         return -EPERM;
-
-//     /* set mode to "dedicated" */
-//     set_bit(WQ_FLAG_DEDICATED, &wq->flags);
-//     wq->threshold = 0;
-
-//     /* only setting up 1 wq, so give it all the wq space */
-//     wq->size = idxd->max_wq_size;
-
-//     /* set priority to 10 */
-//     wq->priority = 10;
-
-//     /* set type to "kernel" */
-//     wq->type = IDXD_WQT_KERNEL;
-
-//     /* set wq group to 0 */
-//     group = idxd->groups[1];
-//     wq->group = group;
-//     group->num_wqs++;
-
-//     /* set name to "iaa_crypto" */
-//     memset(wq->name, 0, WQ_NAME_SIZE + 1);
-//     strscpy(wq->name, "dmaengine", WQ_NAME_SIZE + 1);
-
-//     /* set driver_name to "crypto" */
-//     memset(wq->driver_name, 0, DRIVER_NAME_SIZE + 1);
-//     strscpy(wq->driver_name, "dmaengine", DRIVER_NAME_SIZE + 1);
-
-//     engine = idxd->engines[1];
-
-//     /* set engine group to 0 */
-//     engine->group = idxd->groups[1];
-//     engine->group->num_engines++;
-
-//     return 0;
-// }
+static char *kmalloc_area3;
+static char *kmalloc_area4;
 
 static int init_dsa(void)
 {
@@ -244,7 +58,6 @@ static int init_dsa(void)
         pci_dev_list[device_count] = pci_dev;
         device_count++;
         struct device *dev = &pci_dev->dev;
-        pr_info("device name: %s\n", dev_name(dev));
         if (device_count >= 8)
             break;
     }
@@ -254,7 +67,6 @@ static int init_dsa(void)
         pr_err("could not find 8 DSA devices\n");
         return -1;
     }
-    pr_info("device count: %d\n", device_count);
 
     dev = &pci_dev_list[1]->dev;
 
@@ -269,21 +81,13 @@ static int init_dsa(void)
     {
         pr_err("idxd device is not configurable\n");
     }
-
-    pr_info("idxd->hw.version: %d\n", idxd_device->hw.version);
-    pr_info("idxd pasid: %d\n", idxd_device->pasid);
-
-    pr_info("idxd wq 1 state: %d\n", idxd_device->wqs[1]->state);
+    if (idxd_device->wqs[1]->state != IDXD_WQ_ENABLED)
+    {
+        pr_err("idxd wq 1 is not enabled\n");
+        return -1;
+    }
 
     wq = idxd_device->wqs[1];
-    if (wq_pasid_enabled(wq))
-    {
-        pr_info("wq pasid enabled\n");
-    }
-    else
-    {
-        pr_info("wq pasid not enabled\n");
-    }
 
     if (is_idxd_wq_dmaengine(wq))
     {
@@ -294,46 +98,39 @@ static int init_dsa(void)
         pr_info("wq is not dmaengine\n");
         return 0;
     }
-    // idxd_wq_get(wq);
+
     chan = dma_get_slave_channel(&wq->idxd_chan->chan);
-    pr_info("client count: %d\n", wq->client_count);
 
     pr_info("wq num_descs: %d\n", wq->num_descs);
 
     // dma infos
-    pr_info("dma info\n");
     pr_info("dma device name: %s\n", dma_chan_name(&wq->idxd_chan->chan));
 
-    // wq->flags = 0;
     pr_info("wq flags: 0x%x\n", wq->flags);
-    // set_bit(WQ_FLAG_DEDICATED, &wq->flags);
-
-    // pr_info("before wq flags: 0x%x\n", wq->flags);
     clear_bit(WQ_FLAG_ATS_DISABLE, &wq->flags);
     clear_bit(WQ_FLAG_PRS_DISABLE, &wq->flags);
-    // clear_bit(WQ_FLAG_BLOCK_ON_FAULT, &wq->flags);
     pr_info("after wq flags: 0x%x\n", wq->flags);
 
-    // sva = iommu_sva_bind_device(dev, current->mm);
-    // if (IS_ERR(sva))
-    // {
-    //     pr_info("iommu_sva_bind_device failed\n");
-    // }
-    // else
-    // {
-    //     pr_info("iommu_sva_bind_device success\n");
-    // }
-    // pasid = iommu_sva_get_pasid(sva);
+    sva = iommu_sva_bind_device(dev, current->mm);
+    if (IS_ERR(sva))
+    {
+        pr_info("iommu_sva_bind_device failed\n");
+    }
+    else
+    {
+        pr_info("iommu_sva_bind_device success\n");
+    }
+    pasid = iommu_sva_get_pasid(sva);
 
-    // pr_info("pasid: %d\n", pasid);
-    // if (idxd_wq_set_pasid(wq, pasid) < 0)
-    // {
-    //     pr_info("idxd_wq_set_pasid failed\n");
-    // }
-    // else
-    // {
-    //     pr_info("idxd_wq_set_pasid success\n");
-    // }
+    pr_info("pasid: %d\n", pasid);
+    if (idxd_wq_set_pasid(wq, pasid) < 0)
+    {
+        pr_info("idxd_wq_set_pasid failed\n");
+    }
+    else
+    {
+        pr_info("idxd_wq_set_pasid success\n");
+    }
 
     return 0;
 }
@@ -341,31 +138,13 @@ static int init_cdev(void)
 {
     int ret = 0;
     int i;
-    /* TODO 3/7: create a new entry in procfs */
-    // struct proc_dir_entry *entry;
-
-    // // This is used to create a new file in /proc
-    // entry = proc_create(PROC_KMALLOC_NAME, 0, NULL, &my_proc_ops);
-    // if (!entry)
-    // {
-    //     ret = -ENOMEM;
-    //     goto out;
-    // }
-
-    // // This is used to register a character device
-    // // The major number is MY_MAJOR
-    // // The minor number is 0
-    // // The name of the device is "mymmap"
-    // ret = register_chrdev_region(MKDEV(MY_MAJOR, 2), 1, "mykmap");
-    // if (ret < 0)
-    // {
-    //     pr_err("could not register region\n");
-    //     goto out_no_chrdev;
-    // }
 
     /* TODO 1/6: allocate NPAGES using kmalloc */
     kmalloc_area = (char *)kmalloc(NPAGES * PAGE_SIZE, GFP_KERNEL);
     kmalloc_area2 = (char *)kmalloc(NPAGES * PAGE_SIZE, GFP_KERNEL);
+    kmalloc_area3 = (char *)kmalloc(NPAGES * PAGE_SIZE, GFP_KERNEL);
+    kmalloc_area4 = (char *)kmalloc(NPAGES * PAGE_SIZE, GFP_KERNEL);
+
     if (kmalloc_area == NULL)
     {
         ret = -ENOMEM;
@@ -378,23 +157,39 @@ static int init_cdev(void)
         pr_err("could not allocate memory\n");
         goto out_kfree;
     }
+    if (kmalloc_area3 == NULL)
+    {
+        ret = -ENOMEM;
+        pr_err("could not allocate memory\n");
+        goto out_kfree;
+    }
+    if (kmalloc_area4 == NULL)
+    {
+        ret = -ENOMEM;
+        pr_err("could not allocate memory\n");
+        goto out_kfree;
+    }
 
     /* TODO 1/2: mark pages as reserved */
     for (i = 0; i < NPAGES * PAGE_SIZE; i += PAGE_SIZE)
+    {
         SetPageReserved(virt_to_page(kmalloc_area + i));
-
-    for (i = 0; i < NPAGES * PAGE_SIZE; i += PAGE_SIZE)
         SetPageReserved(virt_to_page(kmalloc_area2 + i));
+        SetPageReserved(virt_to_page(kmalloc_area3 + i));
+        SetPageReserved(virt_to_page(kmalloc_area4 + i));
+    }
 
-    // So i want to check how much distance between each page
-    // Get the page frame number of each page
-    // Then calculate the distance between each page
     for (i = 0; i < NPAGES * PAGE_SIZE; i += PAGE_SIZE)
     {
         unsigned long pfn = page_to_pfn(virt_to_page(kmalloc_area + i));
-        pr_info("area 1: %lu\n", pfn);
+        // pr_info("area 1: %lu\n", pfn);
         pfn = page_to_pfn(virt_to_page(kmalloc_area2 + i));
-        pr_info("area 2: %lu\n", pfn);
+        // pr_info("area 2: %lu\n", pfn);
+
+        pfn = page_to_pfn(virt_to_page(kmalloc_area3 + i));
+        // pr_info("area 3: %lu\n", pfn);
+        pfn = page_to_pfn(virt_to_page(kmalloc_area4 + i));
+        // pr_info("area 4: %lu\n", pfn);
     }
 
     /* TODO 1/6: write data in each page */
@@ -403,22 +198,17 @@ static int init_cdev(void)
         // Write kmalloc in each page
         sprintf(kmalloc_area + i, "kmalloc %ld", i / PAGE_SIZE);
         sprintf(kmalloc_area2 + i, "kmalloc2 %ld", i / PAGE_SIZE);
+        sprintf(kmalloc_area3 + i, "kmalloc3 %ld", i / PAGE_SIZE);
+        sprintf(kmalloc_area4 + i, "kmalloc4 %ld", i / PAGE_SIZE);
 
         // Last of the String is \0
         // After the null i will fill it with 1
         // So i can check if the data is written correctly
         memset(kmalloc_area + i + strlen(kmalloc_area + i) + 1, '0', PAGE_SIZE - strlen(kmalloc_area + i) - 1);
         memset(kmalloc_area2 + i + strlen(kmalloc_area2 + i) + 1, '0', PAGE_SIZE - strlen(kmalloc_area2 + i) - 1);
+        memset(kmalloc_area3 + i + strlen(kmalloc_area3 + i) + 1, '0', PAGE_SIZE - strlen(kmalloc_area3 + i) - 1);
+        memset(kmalloc_area4 + i + strlen(kmalloc_area4 + i) + 1, '0', PAGE_SIZE - strlen(kmalloc_area4 + i) - 1);
     }
-
-    // cdev_init(&mmap_cdev, &mmap_fops);
-    // mmap_cdev.owner = THIS_MODULE;
-    // ret = cdev_add(&mmap_cdev, MKDEV(MY_MAJOR, 0), 1);
-    // if (ret < 0)
-    // {
-    //     pr_err("could not add device\n");
-    //     goto out_kfree;
-    // }
 
     pr_info("mykmap module loaded\n");
 
@@ -426,11 +216,10 @@ static int init_cdev(void)
 
 out_kfree:
     kfree(kmalloc_area);
-    // out_unreg:
-    //     unregister_chrdev_region(MKDEV(MY_MAJOR, 2), 1);
-    // out_no_chrdev:
-    //     remove_proc_entry(PROC_KMALLOC_NAME, NULL);
-    // out:
+    kfree(kmalloc_area2);
+    kfree(kmalloc_area3);
+    kfree(kmalloc_area4);
+
     return ret;
 }
 
@@ -440,6 +229,7 @@ static void dsa_copy(void)
     struct idxd_desc *desc = NULL;
     struct dsa_hw_desc *hw = NULL;
     struct dma_device *device = &idxd_device->idxd_dma->dma;
+    dma_addr_t src1, src2, dst1, dst2;
     int cmp = 0;
     int poll = 0;
     int rc = 0;
@@ -449,18 +239,21 @@ static void dsa_copy(void)
         pr_info("error\n");
     }
 
-    dma_addr_t src = dma_map_single(device->dev, kmalloc_area, NPAGES * PAGE_SIZE, DMA_BIDIRECTIONAL);
-    if (dma_mapping_error(chan->device->dev, src))
-    {
-        pr_info("dma_map_single error\n");
-    }
-    dma_addr_t dst = dma_map_single(device->dev, kmalloc_area2, NPAGES * PAGE_SIZE, DMA_BIDIRECTIONAL);
-    if (dma_mapping_error(chan->device->dev, dst))
-    {
-        pr_info("dma_map_single error\n");
-    }
+    // src1 = dma_map_single(device->dev, kmalloc_area, NPAGES * PAGE_SIZE, DMA_BIDIRECTIONAL);
+    // if (dma_mapping_error(chan->device->dev, src))
+    // {
+    //     pr_info("dma_map_single error\n");
+    // }
+    // dst1 = dma_map_single(device->dev, kmalloc_area2, NPAGES * PAGE_SIZE, DMA_BIDIRECTIONAL);
+    // if (dma_mapping_error(chan->device->dev, dst))
+    // {
+    //     pr_info("dma_map_single error\n");
+    // }
 
-    struct dma_async_tx_descriptor *dma_desc = device->device_prep_dma_memcpy(chan, dst, src, NPAGES * PAGE_SIZE, IDXD_OP_FLAG_RCR | IDXD_OP_FLAG_CRAV | IDXD_OP_FLAG_CC);
+    src1 = (dma_addr_t)kmalloc_area;
+    dst1 = (dma_addr_t)kmalloc_area2;
+
+    struct dma_async_tx_descriptor *dma_desc = device->device_prep_dma_memcpy(chan, dst1, src1, NPAGES * PAGE_SIZE, IDXD_OP_FLAG_RCR | IDXD_OP_FLAG_CRAV | IDXD_OP_FLAG_CC);
     if (!dma_desc)
     {
         pr_info("device_prep_dma_memcpy error\n");
@@ -474,14 +267,12 @@ static void dsa_copy(void)
     pr_info("completion_addr: 0x%llx\n", hw->completion_addr);
 
     pr_info("desc address: 0x%llx\n", desc->compl_dma);
-    pr_info("hw priv: %d\n", hw->priv);
 
     pr_info("init status: 0x%x\n", desc->completion->status);
 
     pr_info("xfer_size: %d\n", desc->hw->xfer_size);
 
     desc->txd.cookie = desc->txd.tx_submit(&desc->txd);
-    pr_info("cookie: %d\n", desc->txd.cookie);
 
     while (!desc->completion->status && poll++ < POLL_RETRY_MAX)
     {
@@ -494,14 +285,10 @@ static void dsa_copy(void)
     if (cmp == 0)
     {
         pr_info("copy success\n");
-        pr_info("kmalloc_area: %s\n", kmalloc_area);
-        pr_info("kmalloc_area2: %s\n", kmalloc_area2);
     }
     else
     {
         pr_info("copy fail\n");
-        pr_info("kmalloc_area: %s\n", kmalloc_area);
-        pr_info("kmalloc_area2: %s\n", kmalloc_area2);
     }
     pr_info("fault info: 0x%x\n", desc->completion->fault_info);
     pr_info("result: 0x%x\n", desc->completion->result);
@@ -509,8 +296,8 @@ static void dsa_copy(void)
 
     idxd_desc_complete(desc, IDXD_COMPLETE_NORMAL, 0);
 
-    dma_unmap_single(device->dev, src, NPAGES * PAGE_SIZE, DMA_BIDIRECTIONAL);
-    dma_unmap_single(device->dev, dst, NPAGES * PAGE_SIZE, DMA_BIDIRECTIONAL);
+    // dma_unmap_single(device->dev, src1, NPAGES * PAGE_SIZE, DMA_BIDIRECTIONAL);
+    // dma_unmap_single(device->dev, dst1, NPAGES * PAGE_SIZE, DMA_BIDIRECTIONAL);
 
     return;
 }
@@ -536,8 +323,6 @@ static void exit_cdev(void)
 {
     int i;
 
-    // cdev_del(&mmap_cdev);
-
     /* TODO 1/3: clear reservation on pages and free mem.*/
     for (i = 0; i < NPAGES * PAGE_SIZE; i += PAGE_SIZE)
         ClearPageReserved(virt_to_page(kmalloc_area + i));
@@ -547,9 +332,13 @@ static void exit_cdev(void)
         ClearPageReserved(virt_to_page(kmalloc_area2 + i));
     kfree(kmalloc_area2);
 
-    // unregister_chrdev_region(MKDEV(MY_MAJOR, 2), 1);
-    // /* TODO 3: remove proc entry */
-    // remove_proc_entry(PROC_KMALLOC_NAME, NULL);
+    for (i = 0; i < NPAGES * PAGE_SIZE; i += PAGE_SIZE)
+        ClearPageReserved(virt_to_page(kmalloc_area3 + i));
+    kfree(kmalloc_area3);
+
+    for (i = 0; i < NPAGES * PAGE_SIZE; i += PAGE_SIZE)
+        ClearPageReserved(virt_to_page(kmalloc_area4 + i));
+    kfree(kmalloc_area4);
 
     pr_info("myvmap module unloaded\n");
 }
@@ -558,7 +347,6 @@ static void __exit my_exit(void)
 {
     exit_cdev();
     dma_release_channel(chan);
-    // idxd_wq_put(wq);
 }
 
 module_init(my_init);
