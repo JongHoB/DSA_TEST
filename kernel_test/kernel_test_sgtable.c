c957613e5914a33f4c09c7067fb17de37caa847e
JongHoB <qorwhdgh@gmail.com>
2024-02-22 05:29:30 +0000

    dma_unmap_sgtable loc 1

--- kernel_test/kernel_test_sgtable.c
+++ kernel_test/kernel_test_sgtable.c
@@ -35,6 +35,8 @@ MODULE_AUTHOR("Jongho Baik");
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_IMPORT_NS(IDXD);
 
+LIST_HEAD(idxd_desc_lists);
+
 static struct dma_chan *chan = NULL;
 static struct idxd_wq *wq;
 struct idxd_device *idxd_device;
@@ -45,11 +47,15 @@ static char *vmalloc_area;
 static char *vmalloc_area2;
 static char *vmalloc_area3;
 static char *vmalloc_area4;
+static char *vmalloc_area5;
+static char *vmalloc_area6;
 
 struct sg_table *sgt1;
 struct sg_table *sgt2;
 struct sg_table *sgt3;
 struct sg_table *sgt4;
+
+struct timespec64 start, end, start2, start3, end3, start4, end4, start5, end5, start6, end6, start7, end7, start8, end8, start9, end9, start10, end10, start11, end11;
 
 static int init_dsa(void)
 {
@@ -142,11 +148,15 @@ static int init_region(void)
     int ret = 0;
     int i;
 
+    pr_info("transfer size: %d\n", NPAGES * PAGE_SIZE);
+
     /* TODO 1/6: allocate NPAGES using kmalloc */
     vmalloc_area = (char *)vmalloc(NPAGES * PAGE_SIZE);
     vmalloc_area2 = (char *)vmalloc(NPAGES * PAGE_SIZE);
     vmalloc_area3 = (char *)vmalloc(NPAGES * PAGE_SIZE);
     vmalloc_area4 = (char *)vmalloc(NPAGES * PAGE_SIZE);
+    vmalloc_area5 = (char *)vmalloc(NPAGES * PAGE_SIZE);
+    vmalloc_area6 = (char *)vmalloc(NPAGES * PAGE_SIZE);
 
     if (vmalloc_area == NULL || vmalloc_area2 == NULL || vmalloc_area3 == NULL || vmalloc_area4 == NULL)
     {
@@ -162,6 +172,8 @@ static int init_region(void)
         SetPageReserved(vmalloc_to_page(vmalloc_area2 + i));
         SetPageReserved(vmalloc_to_page(vmalloc_area3 + i));
         SetPageReserved(vmalloc_to_page(vmalloc_area4 + i));
+        SetPageReserved(vmalloc_to_page(vmalloc_area5 + i));
+        SetPageReserved(vmalloc_to_page(vmalloc_area6 + i));
     }
 
     for (i = 0; i < NPAGES * PAGE_SIZE; i += PAGE_SIZE)
@@ -174,6 +186,10 @@ static int init_region(void)
         // pr_info("area 3: %lu\n", pfn);
         pfn = vmalloc_to_pfn(vmalloc_area4 + i);
         // pr_info("area 4: %lu\n", pfn);
+        pfn = vmalloc_to_pfn(vmalloc_area5 + i);
+        // pr_info("area 5: %lu\n", pfn);
+        pfn = vmalloc_to_pfn(vmalloc_area6 + i);
+        // pr_info("area 6: %lu\n", pfn);
     }
 
     for (i = 0; i < NPAGES * PAGE_SIZE; i += PAGE_SIZE)
@@ -183,6 +199,8 @@ static int init_region(void)
         sprintf(vmalloc_area2 + i, "vmalloc2 %ld", i / PAGE_SIZE);
         sprintf(vmalloc_area3 + i, "vmalloc3 %ld", i / PAGE_SIZE);
         sprintf(vmalloc_area4 + i, "vmalloc4 %ld", i / PAGE_SIZE);
+        sprintf(vmalloc_area5 + i, "vmalloc5 %ld", i / PAGE_SIZE);
+        sprintf(vmalloc_area6 + i, "vmalloc6 %ld", i / PAGE_SIZE);
 
         // Last of the String is \0
         // After the null i will fill it with 1
@@ -191,6 +209,8 @@ static int init_region(void)
         memset(vmalloc_area2 + i + strlen(vmalloc_area2 + i) + 1, '0', PAGE_SIZE - strlen(vmalloc_area2 + i) - 1);
         memset(vmalloc_area3 + i + strlen(vmalloc_area3 + i) + 1, '0', PAGE_SIZE - strlen(vmalloc_area3 + i) - 1);
         memset(vmalloc_area4 + i + strlen(vmalloc_area4 + i) + 1, '0', PAGE_SIZE - strlen(vmalloc_area4 + i) - 1);
+        memset(vmalloc_area5 + i + strlen(vmalloc_area5 + i) + 1, '0', PAGE_SIZE - strlen(vmalloc_area5 + i) - 1);
+        memset(vmalloc_area6 + i + strlen(vmalloc_area6 + i) + 1, '0', PAGE_SIZE - strlen(vmalloc_area6 + i) - 1);
     }
 
     return 0;
@@ -200,6 +220,8 @@ out_vfree:
     vfree(vmalloc_area2);
     vfree(vmalloc_area3);
     vfree(vmalloc_area4);
+    vfree(vmalloc_area5);
+    vfree(vmalloc_area6);
 
     return ret;
 }
@@ -213,7 +235,7 @@ static void vmalloc_to_sgtable(void)
     sgt1 = kzalloc(sizeof(*sgt1), GFP_KERNEL);
     sgt2 = kzalloc(sizeof(*sgt2), GFP_KERNEL);
 
-    if (sg_alloc_table(sgt1, NPAGES, GFP_KERNEL) || sg_alloc_table(sgt2, NPAGES, GFP_KERNEL) || sg_alloc_table(sgt3, NPAGES, GFP_KERNEL) || sg_alloc_table(sgt4, NPAGES, GFP_KERNEL))
+    if (sg_alloc_table(sgt1, NPAGES, GFP_KERNEL) || sg_alloc_table(sgt2, NPAGES, GFP_KERNEL))
     {
         pr_info("sg_alloc_table failed\n");
         goto free_sgt;
@@ -228,7 +250,7 @@ static void vmalloc_to_sgtable(void)
         }
         sg_set_page(sg, page, PAGE_SIZE, 0);
     }
-    pr_info("sgt1 nents: %d\n sgt1 orig_nents: %d\n", sgt1->nents, sgt1->orig_nents);
+    // pr_info("sgt1 nents: %d\n sgt1 orig_nents: %d\n", sgt1->nents, sgt1->orig_nents);
     for_each_sgtable_sg(sgt2, sg, i)
     {
         page = vmalloc_to_page(vmalloc_area2 + i * PAGE_SIZE);
@@ -238,7 +260,7 @@ static void vmalloc_to_sgtable(void)
         }
         sg_set_page(sg, page, PAGE_SIZE, 0);
     }
-    pr_info("sgt2 nents: %d\n sgt2 orig_nents: %d\n", sgt2->nents, sgt2->orig_nents);
+    // pr_info("sgt2 nents: %d\n sgt2 orig_nents: %d\n", sgt2->nents, sgt2->orig_nents);
 
     return;
 
@@ -253,30 +275,115 @@ static void sgtable_to_dma_map(void)
 {
     int ret;
 
+    ktime_get_ts64(&start);
     ret = dma_map_sgtable(dev, sgt1, DMA_TO_DEVICE, 0);
+    ktime_get_ts64(&end);
     if (ret)
     {
         pr_info("dma_map_sgtable failed\n");
         return;
     }
+    ktime_get_ts64(&start5);
     ret = dma_map_sgtable(dev, sgt2, DMA_FROM_DEVICE, 0);
+    ktime_get_ts64(&end5);
     if (ret)
     {
         pr_info("dma_map_sgtable failed\n");
         return;
     }
+    // pr_info("sgt2 nents: %d\n sgt2 orig_nents: %d\n", sgt2->nents, sgt2->orig_nents);
+    return;
+}
+
+static void test(void)
+{
+    // make vmalloc area to sgtable
+    int i;
+    int ret;
+    struct scatterlist *sg;
+    struct page *page;
+    sgt3 = kzalloc(sizeof(*sgt3), GFP_KERNEL);
+    sgt4 = kzalloc(sizeof(*sgt4), GFP_KERNEL);
+
+    if (sg_alloc_table(sgt3, NPAGES, GFP_KERNEL) || sg_alloc_table(sgt4, NPAGES, GFP_KERNEL))
+    {
+        pr_info("sg_alloc_table failed\n");
+        goto free_sgt;
+    }
+
+    for_each_sgtable_sg(sgt3, sg, i)
+    {
+        page = vmalloc_to_page(vmalloc_area5 + i * PAGE_SIZE);
+        if (!page)
+        {
+            goto free_sgt;
+        }
+        sg_set_page(sg, page, PAGE_SIZE, 0);
+    }
+    // pr_info("sgt1 nents: %d\n sgt1 orig_nents: %d\n", sgt1->nents, sgt1->orig_nents);
+    for_each_sgtable_sg(sgt4, sg, i)
+    {
+        page = vmalloc_to_page(vmalloc_area6 + i * PAGE_SIZE);
+        if (!page)
+        {
+            goto free_sgt;
+        }
+        sg_set_page(sg, page, PAGE_SIZE, 0);
+    }
+    // pr_info("sgt2 nents: %d\n sgt2 orig_nents: %d\n", sgt2->nents, sgt2->orig_nents);
+
+    ktime_get_ts64(&start6);
+    ret = dma_map_sgtable(dev, sgt3, DMA_TO_DEVICE, 0);
+    ktime_get_ts64(&end6);
+    if (ret)
+    {
+        pr_info("dma_map_sgtable failed\n");
+        goto free_sgt;
+    }
+    ktime_get_ts64(&start7);
+    ret = dma_map_sgtable(dev, sgt4, DMA_FROM_DEVICE, 0);
+    ktime_get_ts64(&end7);
+    if (ret)
+    {
+        pr_info("dma_map_sgtable failed\n");
+        goto free_sgt;
+    }
+
+    pr_info("map3 time: %lld\n", timespec64_to_ns(&end6) - timespec64_to_ns(&start6));
+    pr_info("map4 time: %lld\n", timespec64_to_ns(&end7) - timespec64_to_ns(&start7));
+
+    ktime_get_ts64(&start10);
+    dma_unmap_sgtable(dev, sgt3, DMA_TO_DEVICE, 0);
+    ktime_get_ts64(&end10);
+    ktime_get_ts64(&start11);
+    dma_unmap_sgtable(dev, sgt4, DMA_FROM_DEVICE, 0);
+    ktime_get_ts64(&end11);
+
+    pr_info("unmap1 time: %lld\n", timespec64_to_ns(&end10) - timespec64_to_ns(&start10));
+    pr_info("unmap2 time: %lld\n", timespec64_to_ns(&end11) - timespec64_to_ns(&start11));
+
+    return;
+
+free_sgt:
+    sg_free_table(sgt3);
+    sg_free_table(sgt4);
+
     return;
 }
 
 static void dsa_copy(void)
 {
     struct idxd_desc *idxd_desc = NULL;
+    struct idxd_desc_list *desc_list, *desc_entry, *desc_entry_temp;
     struct dsa_hw_desc *hw = NULL;
     struct dma_device *device = &idxd_device->idxd_dma->dma;
     dma_addr_t src1, src2, dst1, dst2;
-    struct timespec64 start, end, start2, end2, start3, end3, start4, end4;
+
+    struct scatterlist *sg_src, *sg_dst;
+    int i;
     int cmp = 0;
     int poll = 0;
+    int poll_entry = 0;
     int ret = 0;
     int fault = 0;
     int nents = 0;
@@ -287,89 +394,101 @@ static void dsa_copy(void)
 
     if (sgt1->nents > 1 && sgt2->nents > 1)
     {
-        idxd_desc = idxd_desc_dma_submit_memcpy_sg(chan, sgt2, sgt1, min_t(unsigned int, sgt1->nents, sgt2->nents), IDXD_OP_FLAG_RCR | IDXD_OP_FLAG_CRAV | IDXD_OP_FLAG_CC | IDXD_OP_FLAG_BOF);
-        pr_info("sg1 nents: %d\n", sgt1->nents);
-        pr_info("sg2 nents: %d\n", sgt2->nents);
+        nents = sgt1->nents > sgt2->nents ? sgt2->nents : sgt1->nents;
+
+        for_each_2_sgtable_dsa_sg(sgt1, sgt2, sg_src, sg_dst, nents, i)
+        {
+            desc_list = (struct idxd_desc_list *)kmalloc(sizeof(struct idxd_desc_list), GFP_KERNEL);
+            desc_list->desc = idxd_desc_dma_submit_memcpy(chan, sg_dma_address(sg_dst), sg_dma_address(sg_src), min_t(unsigned int, sg_dma_len(sg_src), sg_dma_len(sg_dst)), IDXD_OP_FLAG_RCR | IDXD_OP_FLAG_CRAV | IDXD_OP_FLAG_CC | IDXD_OP_FLAG_BOF);
+            desc_list->completion = 0;
+            list_add_tail(&desc_list->list, &idxd_desc_lists);
+        }
     }
     else
     {
-        idxd_desc = idxd_desc_dma_submit_memcpy(chan, sg_dma_address(sgt2->sgl), sg_dma_address(sgt1->sgl), min_t(unsigned int, sg_dma_len(sgt1->sgl), sg_dma_len(sgt2->sgl)), IDXD_OP_FLAG_RCR | IDXD_OP_FLAG_CRAV | IDXD_OP_FLAG_CC | IDXD_OP_FLAG_BOF);
-        pr_info("xfer_size: %d\n", idxd_desc->hw->xfer_size);
+        nents = 1;
+        desc_list = (struct idxd_desc_list *)kmalloc(sizeof(struct idxd_desc_list), GFP_KERNEL);
+        desc_list->desc = idxd_desc_dma_submit_memcpy(chan, sg_dma_address(sgt2->sgl), sg_dma_address(sgt1->sgl), min_t(unsigned int, sg_dma_len(sgt1->sgl), sg_dma_len(sgt2->sgl)), IDXD_OP_FLAG_RCR | IDXD_OP_FLAG_CRAV | IDXD_OP_FLAG_CC | IDXD_OP_FLAG_BOF);
+        desc_list->completion = 0;
+        list_add_tail(&desc_list->list, &idxd_desc_lists);
     }
+    // pr_info("nents: %d\n", nents);
 
-    if (IS_ERR(idxd_desc))
-    {
-        dev_dbg(dev, "Failed to allocate descriptor\n");
-        dev_dbg(dev, "Error code: %ld\n", PTR_ERR(idxd_desc));
-        return;
-    }
-
-    hw = idxd_desc->hw;
-
-    pr_info("opcode: 0x%x\nflags: 0x%x\ninit status: 0x%x\n", hw->opcode, hw->flags, idxd_desc->completion->status);
-
-retry:
-    // pr_info("xfer_size: %d\n", idxd_desc->hw->xfer_size);
-
-    // ret = idxd_submit_desc(wq, idxd_desc);
-    // if (ret)
-    // {
-    //     pr_info("submit failed\n");
-    //     goto out;
-    // }
     ktime_get_ts64(&start3);
-    idxd_submit_desc(wq, idxd_desc);
 
-    while (!idxd_desc->completion->status && poll++ < POLL_RETRY_MAX)
+    list_for_each_entry(desc_entry, &idxd_desc_lists, list)
     {
+        ret = idxd_submit_desc(wq, desc_entry->desc);
+        if (ret)
+        {
+            pr_info("submit failed\n");
+            goto out;
+        }
+    }
+
+    while (poll++ < POLL_RETRY_MAX && poll_entry < nents)
+    {
+        list_for_each_entry(desc_entry, &idxd_desc_lists, list)
+        {
+            if (desc_entry->completion)
+            {
+                continue;
+            }
+            if (desc_entry->desc->completion->status == DSA_COMP_SUCCESS)
+            {
+                desc_entry->completion = 1;
+                poll_entry++;
+            }
+            else if (desc_entry->desc->completion->status)
+            {
+                desc_entry->completion = 1;
+                pr_info("after status: 0x%x\nfault info: 0x%x\n", desc_entry->desc->completion->status, desc_entry->desc->completion->fault_info);
+                pr_info("result: 0x%x\ninvalid flag uint32: 0x%x\n", desc_entry->desc->completion->result, desc_entry->desc->completion->invalid_flags);
+                poll_entry++;
+            }
+        }
         cpu_relax();
     }
     ktime_get_ts64(&end3);
+
     if (poll >= POLL_RETRY_MAX || fault >= FAULT_RETRY_MAX)
     {
         pr_info("poll retry max\n");
-        goto done;
     }
 
-    if (idxd_desc->completion->status != DSA_COMP_SUCCESS)
-    {
-        fault++;
-    }
-    else
-    {
-        pr_info("desc success\n");
-        pr_info("poll: %d\nfault: %d\n", poll, fault);
-    }
-done:
-    pr_info("after status: 0x%x\nfault info: 0x%x\n", idxd_desc->completion->status, idxd_desc->completion->fault_info);
-    pr_info("result: 0x%x\ninvalid flag uint32: 0x%x\n", idxd_desc->completion->result, idxd_desc->completion->invalid_flags);
-
+    pr_info("desc success\n");
     pr_info("poll: %d\nfault: %d\n", poll, fault);
-
-    // idxd_desc_complete(idxd_desc, IDXD_COMPLETE_NORMAL, 0);
-
-    // cmp = memcmp(vmalloc_area, vmalloc_area2, NPAGES * PAGE_SIZE);
-    // cmp ? pr_info("copy fail\n") : pr_info("copy success\n");
 
     cmp = memcmp(vmalloc_area, vmalloc_area2, NPAGES * PAGE_SIZE);
     cmp ? pr_info("DSA FAIL\n") : pr_info("DSA Success\n");
-    // pr_info("page1: %s\n", page_address(page1));
-    // pr_info("page2: %s\n", page_address(page2));
+    // pr_info("1st page %s\n", page_address(vmalloc_to_page(vmalloc_area + 20 * PAGE_SIZE)));
+    // pr_info("2nd page %s\n", page_address(vmalloc_to_page(vmalloc_area2 + 20 * PAGE_SIZE)));
 
     // memmove with CPU
-    // pr_info("memmove start\n");
+    pr_info("memmove start\n");
 
-    // ktime_get_ts64(&start4);
-    // memcpy(page_address(page4), page_address(page3), PAGE_SIZE * int_pow(2, PAGE_ORDER));
-    // ktime_get_ts64(&end4);
+    ktime_get_ts64(&start4);
+    for (int i = 0; i < NPAGES * PAGE_SIZE; i += PAGE_SIZE)
+    {
+        memmove(page_address(vmalloc_to_page(vmalloc_area4 + i)), page_address(vmalloc_to_page(vmalloc_area3 + i)), PAGE_SIZE);
+    }
+    ktime_get_ts64(&end4);
 
-    // cmp = memcmp(page_address(page4), page_address(page3), PAGE_SIZE * int_pow(2, PAGE_ORDER));
-    // cmp ? pr_info("memmove copy fail\n") : pr_info("memmove copy success\n");
+    cmp = memcmp(vmalloc_area3, vmalloc_area4, PAGE_SIZE * NPAGES);
+    cmp ? pr_info("memmove copy fail\n") : pr_info("memmove copy success\n");
 
-    // pr_info("src1 map time1: %lld\n", timespec64_to_ns(&end) - timespec64_to_ns(&start));
-    // pr_info("dest1 map time2: %lld\n", timespec64_to_ns(&end2) - timespec64_to_ns(&start2));
-    // pr_info("DSA memmove time3: %lld\n", timespec64_to_ns(&end3) - timespec64_to_ns(&start3));
-    // pr_info("memmove time4: %lld\n", timespec64_to_ns(&end4) - timespec64_to_ns(&start4));
+    pr_info("src map time1: %lld\n", timespec64_to_ns(&end) - timespec64_to_ns(&start));
+    pr_info("dest map time2: %lld\n", timespec64_to_ns(&end5) - timespec64_to_ns(&start5));
+    pr_info("DSA end to end time: %lld\n", timespec64_to_ns(&end3) - timespec64_to_ns(&start2));
+    pr_info("DSA memmove time3: %lld\n", timespec64_to_ns(&end3) - timespec64_to_ns(&start3));
+    pr_info("memmove time4: %lld\n", timespec64_to_ns(&end4) - timespec64_to_ns(&start4));
+
+    list_for_each_entry_safe(desc_entry, desc_entry_temp, &idxd_desc_lists, list)
+    {
+        idxd_desc_complete(desc_entry->desc, IDXD_COMPLETE_NORMAL, 0);
+        list_del(&desc_entry->list);
+        kfree(desc_entry);
+    }
 
 out:
     ///////////////////////
@@ -379,12 +498,23 @@ out:
     // dma_unmap_single(device->dev, src1, NPAGES * PAGE_SIZE, DMA_BIDIRECTIONAL);
     // dma_unmap_single(device->dev, dst1, NPAGES * PAGE_SIZE, DMA_BIDIRECTIONAL);
 
+    ktime_get_ts64(&start8);
+    dma_unmap_sgtable(dev, sgt1, DMA_TO_DEVICE, 0);
+    ktime_get_ts64(&end8);
+    ktime_get_ts64(&start9);
+    dma_unmap_sgtable(dev, sgt2, DMA_FROM_DEVICE, 0);
+    ktime_get_ts64(&end9);
+
+    pr_info("unmap3 time: %lld\n", timespec64_to_ns(&end8) - timespec64_to_ns(&start8));
+    pr_info("unmap4 time: %lld\n", timespec64_to_ns(&end9) - timespec64_to_ns(&start9));
+
     return;
 }
 
 static int __init
 my_init(void)
 {
+    INIT_LIST_HEAD(&idxd_desc_lists);
     pr_info("mykmap module loaded\n");
     int init_result = init_region();
     if (init_result < 0)
@@ -392,17 +522,21 @@ my_init(void)
         return init_result;
     }
     pr_info("init_region success\n");
-    vmalloc_to_sgtable();
-    pr_info("vmalloc_to_sgtable success\n");
     int dsa_result = init_dsa();
     if (dsa_result < 0)
     {
         return dsa_result;
     }
     pr_info("init_dsa success\n");
+    test();
+    ktime_get_ts64(&start2);
+    vmalloc_to_sgtable();
+    // pr_info("vmalloc_to_sgtable success\n");
     sgtable_to_dma_map();
-    pr_info("sgtable_to_dma_map success\n");
+    // pr_info("sgtable_to_dma_map success\n");
     dsa_copy();
+    // pr_info("dsa_copy success\n");
+
     return 0;
 }
 
@@ -417,16 +551,26 @@ static void exit_module(void)
         ClearPageReserved(vmalloc_to_page(vmalloc_area2 + i));
         ClearPageReserved(vmalloc_to_page(vmalloc_area3 + i));
         ClearPageReserved(vmalloc_to_page(vmalloc_area4 + i));
+        ClearPageReserved(vmalloc_to_page(vmalloc_area5 + i));
+        ClearPageReserved(vmalloc_to_page(vmalloc_area6 + i));
     }
 
     vfree(vmalloc_area);
     vfree(vmalloc_area2);
     vfree(vmalloc_area3);
     vfree(vmalloc_area4);
+    vfree(vmalloc_area5);
+    vfree(vmalloc_area6);
+
+    sg_free_table(sgt1);
+    sg_free_table(sgt2);
+    sg_free_table(sgt3);
+    sg_free_table(sgt4);
 }
 
 static void __exit my_exit(void)
 {
+    list_del_init(&idxd_desc_lists);
     exit_module();
     dma_release_channel(chan);
     pr_info("myvmap module unloaded\n");
